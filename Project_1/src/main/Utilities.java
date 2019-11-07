80368839577bc02d48d29667a8d4859928fc27f9
DiogoASTeixeira <diogo_alex9@hotmail.com>
2019-11-07 11:23:02 +0000

    Reformed Utilities

--- Project_1/src/main/Utilities.java
+++ Project_1/src/main/Utilities.java
@@ -1,39 +1,58 @@
 package main;
+
 public class Utilities {
 	private final static int NUMBER_OF_TEAMS = 5;
+
+	public enum Outcome {
+		VICTORY,
+		LOSS,
+		SAME_TEAM,
+		NEUTRAL
+	}
 	
-	public static boolean isNeutral(Integer teamNumber, Integer comparisonN) {
-		switch(teamNumber) {
-			case 1:
-				return (comparisonN == 3 || comparisonN == 4);
-			case 2:
-				return (comparisonN == 4 || comparisonN == 5);
-			case 3:
-				return (comparisonN == 5 || comparisonN == 1);
-			case 4:
-				return (comparisonN == 1 || comparisonN == 2);
-			case 5:
-				return (comparisonN == 2 || comparisonN == 3);
-			default:
-				return false;
+	public static Outcome getOutcome(Integer teamA, Integer teamB)
+	{
+		if(killsTheEnemy(teamA, teamB))
+		{
+			return Outcome.VICTORY;
+		}
+		if(diesHorribly(teamA, teamB))
+		{
+			return Outcome.LOSS;
+		}
+		if(isOnSameTeam(teamA, teamB))
+		{
+			return Outcome.SAME_TEAM;
+		}
+		else return Outcome.NEUTRAL;
+	}
+
+	public static Outcome adjustOutcome(Outcome outcome)
+	{
+		switch(outcome)
+		{
+		case VICTORY: return Outcome.LOSS;
+		case LOSS: return Outcome.VICTORY;
+		default: return outcome;
 		}
 	}
 	
-	public static boolean diesHorribly(Integer teamNumber, Integer comparisonN) {
+	private static boolean diesHorribly(Integer teamNumber, Integer comparisonN) {
 		return getPrey(comparisonN) == teamNumber;
 	}
-	
-	public static boolean killsTheEnemy(Integer teamNumber, Integer comparisonN) {
+
+	private static boolean killsTheEnemy(Integer teamNumber, Integer comparisonN) {
 		return getPrey(teamNumber) == comparisonN;
 	}
-	
-	public static boolean isOnMyTeam(Integer teamNumber, Integer comparisonN) {
+
+	private static boolean isOnSameTeam(Integer teamNumber, Integer comparisonN) {
 		return teamNumber == comparisonN;
 	}
-	
+
 	private static int getPrey(int teamNumber) {
-		if(teamNumber == NUMBER_OF_TEAMS)
+		if (teamNumber == NUMBER_OF_TEAMS)
 			return 1;
-		else return teamNumber + 1;
+		else
+			return teamNumber + 1;
 	}
 }
